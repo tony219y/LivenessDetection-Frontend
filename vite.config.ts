73195bbcc08ed1b030d90ae66e2598c1ff78b098
tony007x <aekanan2004@gmail.com>
2025-03-14 14:02:28 +0700

    allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+
   plugins: [
     react(),
     tailwindcss(),
   ],
+  preview: {
+    allowedHosts: ['liveness.tony219y.com'], // เพิ่มโฮสต์ที่ต้องการ
+  },
 })
